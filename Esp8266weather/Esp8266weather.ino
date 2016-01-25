362960d9a92d07000204b79388a50293330a098e
Wero1414 <zorros_grises1414@hotmail.com>
2016-01-24 23:07:27 -0600

    Added IFTTT

--- Esp8266weather/Esp8266weather.ino
+++ Esp8266weather/Esp8266weather.ino
@@ -1,23 +1,34 @@
 #include <ESP8266WiFi.h>
-#include "DHT.h"             
+#include "DHT.h"    
+#define DHTPIN          2   //Pin to attach the DHT
+#define DHTTYPE DHT11       //type of DTH  
 const char* ssid     = "Your SSID"; 
 const char* password = "Your password";
-const int sleepTimeS = 600; //18000 for Half hour
-
-#define DHTPIN          2   
-#define DHTTYPE DHT11                                 
+const int sleepTimeS = 600; //18000 for Half hour, 300 for 5 minutes etc.
+                               
+///////////////Weather//////////////////////// 
 char server [] = "weatherstation.wunderground.com";  
 char WEBPAGE [] = "GET /weatherstation/updateweatherstation.php?";
-char ID [] = "Your weather station ID";
-char PASSWORD [] = "Your password";
+char ID [] = "YourWeatherID";
+char PASSWORD [] = "YourPasswordOfWunderground";
 
 
+/////////////IFTTT///////////////////////
+const char* host = "maker.ifttt.com";//dont change
+const String IFTTT_Event = "YourEventName"; 
+const int puertoHost = 80;
+const String Maker_Key = "YourMakerKey";
+String conexionIF = "POST /trigger/"+IFTTT_Event+"/with/key/"+Maker_Key +" HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n" + 
+                    "Content-Type: application/x-www-form-urlencoded\r\n\r\n";
+//////////////////////////////////////////
 DHT dht(DHTPIN, DHTTYPE);               
            
 
 
-void setup(){
- Serial.begin(9600);
+void setup()
+{
+  Serial.begin(115200);
   dht.begin();
   delay(1000);
   Serial.println();
@@ -25,12 +36,21 @@ void setup(){
   Serial.println(ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-  delay(500);
-  Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
 }
 
 void loop(){  
+  //Check battery
+  int level = analogRead(A0);
+  level = map(level, 0, 1024, 0, 100);
+  if(level<50)
+  { 
+   mandarNot(); //Send IFTT
+   Serial.println("Low batter");
+   delay(500);
+  }
   //Get sensor data
   float tempc = dht.readTemperature(); 
   float tempf =  (tempc * 9.0)/ 5.0 + 32.0; 
@@ -48,6 +68,7 @@ void loop(){
   Serial.println(dewptf);
   Serial.print("humidity= ");
   Serial.println(humidity);
+  
   //Send data to Weather Underground
   Serial.print("connecting to ");
   Serial.println(server);
@@ -77,7 +98,7 @@ void loop(){
 }
 
 
-double dewPoint(double tempf, double humidity)
+double dewPoint(double tempf, double humidity) //Calculate dew Point
 {
   double A0= 373.15/(273.15 + tempf);
   double SUM = -7.90298 * (A0-1);
@@ -88,6 +109,22 @@ double dewPoint(double tempf, double humidity)
   double VP = pow(10, SUM-3) * humidity;
   double T = log(VP/0.61078);   
   return (241.88 * T) / (17.558-T);
+}
+
+void mandarNot(){
+  WiFiClient client;
+  if (!client.connect(host, puertoHost)) //Check connection
+  {
+    Serial.println("Failed connection");
+    return;
+  }
+  client.print(conexionIF);//Send information
+  delay(10);
+  while(client.available())
+  {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
 }
 
 void sleepMode(){
